155a748a66cd0e864cf6f5f022ff7a961535d211
Jose Blanca <jblanca@upv.es>
2024-03-27 10:25:28 +0100

    GenotypicFreqs str moved to repr

--- src/pop_lab/one_locus_two_alleles_simulator.py
+++ src/pop_lab/one_locus_two_alleles_simulator.py
@@ -77,7 +77,7 @@ class GenotypicFreqs:
     def aa(self):
         return self._aa
 
-    def __str__(self):
+    def __repr__(self):
         return f"{self.__class__.__name__}({self.AA}, {self.Aa}, {self.aa})"
 
 
