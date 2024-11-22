55d69c9972a944472dd01def69a6a1d4378bc55c
Jose Blanca <jblanca@upv.es>
2024-11-22 11:39:33 +0100

    update script to export web site

--- cp_to_bioinf_web.sh
+++ cp_to_bioinf_web.sh
@@ -1,1 +1,7 @@
-cp -ra /home/jose/devel/pop_lab/output_site/ /home/jose/webs/bioinf/github_io/pop_lab
+rm -r shiny_site/one_locus
+uv run shinylive export src/one_locus_app/ shiny_site/one_locus/
+rm -r shiny_site/drifting_pops
+uv run shinylive export src/drifting_pops_app/ shiny_site/drifting_pops/
+rm -r shiny_site/bottleneck
+uv run shinylive export src/bottleneck_app/ shiny_site/bottleneck/
+cp -ra /home/jose/devel/pop_lab/shiny_site/* /home/jose/webs/bioinf/github_io/pop_lab/
