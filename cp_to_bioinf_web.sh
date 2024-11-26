fd4714d86ad53e422674068e8c2c90fbf9655d7e
Jose Blanca <jblanca@upv.es>
2024-11-26 11:54:26 +0100

    create balancing selection app

--- cp_to_bioinf_web.sh
+++ cp_to_bioinf_web.sh
@@ -2,7 +2,9 @@ rm -r shiny_site/simple_drift
 uv run shinylive export src/apps/fwd/simple_drift_app/ shiny_site/simple_drift/
 rm -r shiny_site/simple_selection
 uv run shinylive export src/apps/fwd/simple_selection_app/ shiny_site/simple_selection/
-rm -r shiny_site/simple_selection
+rm -r shiny_site/balancing_selection
+uv run shinylive export src/apps/fwd/balancing_selection_app/ shiny_site/balancing_selection/
+rm -r shiny_site/one_locus
 uv run shinylive export src/apps/fwd/one_locus_app/ shiny_site/one_locus/
 rm -r shiny_site/drifting_pops
 uv run shinylive export src/apps/msprime/drifting_pops_app/ shiny_site/drifting_pops/
